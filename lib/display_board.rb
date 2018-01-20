132fc0878576e98c4825373cb8c2095012d2c751
Leann Kim <leannminakim@gmail.com>
2018-01-20 19:46:18 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+output = capture_puts{ display_board(board) }
+rows = output.split("\n")
+
 def display_board(board)
   puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
 end
